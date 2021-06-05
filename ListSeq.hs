34f0a4f6c9a739032129064e68b19d274aa5640e
Pablo Antuña <pablo.antuna99@gmail.com>
2021-06-05 16:02:31 -0300

    first commit

---
+++ ListSeq.hs
@@ -0,0 +1,83 @@
+module ListSeq where
+import Seq
+import Par
+
+instance Seq [] where
+    emptyS = []
+
+    singletonS x = [x]
+
+    lengthS = length
+
+    -- nthS = (!!) -- creo que esta es 100% valida asi lo hacia Damian
+
+    -- creo que esta es la unica forma de mostrar error que no modifique la signatura de la funcion
+    -- pero es la misma forma que usa aldana y me da miedo
+    -- supongo que podemos hacerlo no exhaustivo pero en el otro tp lo preguntaron
+    nthS [] _ = error "No puede accederse a un elemento de una lista vacia"
+    nthS (x:_) 0 = x
+    nthS (_:xs) n = nthS xs (n - 1)
+
+
+    -- copiada de Damian medio chad que use la lambda function
+    tabulateS f n = aux (\x -> f (n-x)) n
+                    where
+                        aux _ 0 = emptyS 
+                        aux f n = let
+                                    (x, xs) = f n ||| aux f (n-1)
+                                  in
+                                    x:xs
+
+    -- no copiada pero resulta que aldana la tiene EXACTAMENTE igual y me da miedo que crean que lo copiamos
+    -- tabulateS f n = aux f n 0 
+    --                 where
+    --                     aux _ 0 _ = emptyS
+    --                     aux f n i = let 
+    --                                   (x, xs) = f i ||| aux f (n-1) (i+1)
+    --                                 in
+    --                                     x:xs
+
+    --- mapS = map -- idk tal vez no se como trabaja internamente map como para saber si optimiza ese pararelismo que quieren
+
+    mapS _ [] = emptyS 
+    mapS f (x:xs) = let
+                        (y, ys) = f x ||| mapS f xs
+                    in
+                        y:ys
+
+    --- filterS = filter same duda map
+    filterS _ [] = emptyS 
+    filterS f (x:xs) = let
+                        (y, ys) = f x ||| filterS f xs
+                      in
+                        if y then x:ys else ys
+
+    -- appendS = (++)
+    appendS xs [] = xs
+    appendS [] ys = ys
+    appendS (x:xs) ys = x : appendS xs ys
+
+    takeS xs n = take n xs
+
+    dropS xs n = drop n xs
+
+    showtS [] = EMPTY
+    showtS [x] = ELT x
+    showtS xs = let
+                    mitad = lengthS xs `div` 2
+                    (left, right) = takeS xs mitad ||| dropS xs mitad
+                in 
+                    NODE left right
+
+    showlS [] = NIL
+    showlS (x:xs) = CONS x xs
+
+
+    -- joinS = concat creo?
+    joinS [] = []
+    joinS (x:xs) = appendS x (joinS xs)
+
+    --reduceS
+    --scanS
+
+    fromList = id
