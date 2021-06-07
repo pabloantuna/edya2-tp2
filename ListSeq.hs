f8eb552d5dfba8ee2634f2fb16f14c2e3a33a26a
Lautaro Garavano <lgaravano28@gmail.com>
2021-06-07 19:09:21 -0300

    added reduce

--- ListSeq.hs
+++ ListSeq.hs
@@ -48,6 +48,25 @@ instance Seq [] where
   joinS = concat
 
   --reduceS
+  reduceS f b [] = b
+  reduceS f b s = f b (reduceS' s)
+    where
+      reduceS' [s1] = s1
+      reduceS' s = reduceS' (contraer s)
+      contraer [] = []
+      contraer [x1] = [x1]
+      contraer (x1:x2:xs) = let (y, ys) = f x1 x2 ||| contraer xs in y:ys
+
   --scanS
+  -- scanS f b [] = ([b], b)
+  -- scanS f b s = expandir s (scanS f b (contraer s))
+  --   where
+  --     contraer [] = []
+  --     contraer [x1] = [x1]
+  --     contraer (x1:x2:xs) = let (y, ys) = f x1 x2 ||| contraer xs in y:ys
+  --     expandir s s'@([], t) = s'
+  --     expandir s s'@([x], t) = s'
+  --     expandir (s1:ss) (x1:x2:xs, t) = (x1 : f s1 x2 : fst expandir ss (xs, t), t)
+
 
   fromList = id
