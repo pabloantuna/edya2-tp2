113c4c74cf0bdaa0f8227259c12feeaf07b7c6f7
Pablo Antuña <pablo.antuna99@gmail.com>
2021-06-07 18:53:51 -0300

    borrado comentarios

--- ListSeq.hs
+++ ListSeq.hs
@@ -12,7 +12,6 @@ instance Seq [] where
 
   nthS = (!!)
 
-  -- tabulateS f n = mapS f [0..n]
   tabulateS f n = fhasta n
     where
       fhasta 0 = emptyS
@@ -20,14 +19,11 @@ instance Seq [] where
         let (x, xs) = f (n - m) ||| fhasta (m -1)
          in x : xs
 
-  --- mapS = map -- idk tal vez no se como trabaja internamente map como para saber si optimiza ese pararelismo que quieren
-
   mapS _ [] = emptyS
   mapS f (x : xs) =
     let (y, ys) = f x ||| mapS f xs
      in y : ys
 
-  --- filterS = filter same duda map
   filterS _ [] = emptyS
   filterS f (x : xs) =
     let (y, ys) = f x ||| filterS f xs
